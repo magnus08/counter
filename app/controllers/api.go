cad81aadcd6cef98c10db4ab17705c89a786f5bf
Magnus Andersson <magnus08@gmail.com>
2017-10-09 16:14:59 +0200

    Playing with arrays.

--- app/controllers/api.go
+++ app/controllers/api.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/revel/revel"
+	"github.com/magnus08/counter/app/event"
 )
 
 type Api struct {
@@ -9,5 +10,6 @@ type Api struct {
 }
 
 func (c Api) ListEvents(user int) revel.Result {
-	return c.RenderJSON(user)
+
+	return c.RenderJSON(event.ListEvents(user))
 }
--- app/event/event.go
+++ app/event/event.go
@@ -9,17 +9,21 @@ type Event struct {
 	User      int
 	Timestamp int
 	Name      string
-	Kcal      int
+	Energy      int
 }
 
-var Events []Event
+var events = []Event{
+	newEvent("aa", 1, "AA name", 100),
+	newEvent("ab", 1, "AB name", 99),
+	newEvent("ac", 1, "AC name", 98),
+	newEvent("b", 1, "B name", 200) }
 
-func newEvent(typ string, user int, name string, kcal int) Event {
-	return Event{typ, user, int(time.Now().Unix()), name, kcal}
+func newEvent(typ string, user int, name string, energy int) Event {
+	return Event{typ, user, int(time.Now().Unix()), name, energy}
 }
 
-func listEvents() []Event {
-	return Events
+func ListEvents(user int) []Event {
+	return events
 }
 
 func init() {
