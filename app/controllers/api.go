908ec77294e1c902b89577b1f60f226f5723d6ee
Magnus Andersson <magnus08@gmail.com>
2017-10-09 17:00:40 +0200

    More experiments.

--- app/controllers/api.go
+++ app/controllers/api.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/revel/revel"
 	"github.com/magnus08/counter/app/event"
 )
@@ -10,6 +11,15 @@ type Api struct {
 }
 
 func (c Api) ListEvents(user int) revel.Result {
+	fmt.Printf("What!\n")
 
 	return c.RenderJSON(event.ListEvents(user))
+}
+
+func (c Api) AddEvent() revel.Result {
+  fmt.Printf("What?")
+  var jsonData map[string]interface{}
+	c.Params.BindJSON(&jsonData)
+	event.AddEvent(event.NewEvent("aa", 1, "AA name", 100))
+	return c.RenderJSON(1)
 }
--- app/event/event.go
+++ app/event/event.go
@@ -13,18 +13,27 @@ type Event struct {
 }
 
 var events = []Event{
-	newEvent("aa", 1, "AA name", 100),
-	newEvent("ab", 1, "AB name", 99),
-	newEvent("ac", 1, "AC name", 98),
-	newEvent("b", 1, "B name", 200) }
+	NewEvent("aa", 1, "AA name", 100),
+	NewEvent("ab", 1, "AB name", 99),
+	NewEvent("ac", 2, "AC name", 98),
+	NewEvent("b", 1, "B name", 200) }
 
-func newEvent(typ string, user int, name string, energy int) Event {
-	return Event{typ, user, int(time.Now().Unix()), name, energy}
-}
+	func NewEvent(typ string, user int, name string, energy int) Event {
+		return Event{typ, user, int(time.Now().Unix()), name, energy}
+	}
 
-func ListEvents(user int) []Event {
-	return events
-}
+	func ListEvents(user int) (ret []Event){
+		for _, v := range events {
+			if(v.User == user) {
+				ret = append(ret, v)
+			}
+		}
+		return ret
+	}
 
-func init() {
-}
+	func AddEvent(event Event) {
+		events = append(events, event)
+	}
+
+	func init() {
+	}
