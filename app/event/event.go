af1aa7c2a2343a5ae126a0ee15997e0df66d6f2d
Magnus Andersson <magnus08@gmail.com>
2017-10-13 16:01:22 +0200

    Renamed json fields + added uuid id.

--- app/event/event.go
+++ app/event/event.go
@@ -2,14 +2,16 @@ package event
 
 import (
 	"time"
+	"github.com/satori/go.uuid"
 )
 
 type Event struct {
-	Type      string
-	User      int
-	Timestamp int
-	Name      string
-	Energy      int
+	Id string `json:"id"`
+	Type string `json:"type"`
+	User int `json:"user"`
+	Timestamp int `json:"timestamp"`
+	Name string `json:"name"`
+	Energy int `json:"energy"`
 }
 
 var events = []Event{
@@ -19,7 +21,8 @@ var events = []Event{
 	NewEvent(1, "b", "B name", 200) }
 
 	func NewEvent(user int, typ string, name string, energy int) Event {
-		return Event{typ, user, int(time.Now().Unix()), name, energy}
+		id := uuid.NewV4()
+		return Event{id.String(), typ, user, int(time.Now().Unix()), name, energy}
 	}
 
 	func ListEvents(user int) (ret []Event){
