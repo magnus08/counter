a7c37762bb1214762965b4ed2fbf883cf5515a43
Magnus Andersson <magnus08@gmail.com>
2017-10-13 15:37:56 +0200

    cors

--- app/init.go
+++ app/init.go
@@ -46,6 +46,8 @@ var HeaderFilter = func(c *revel.Controller, fc []revel.Filter) {
 	c.Response.Out.Header().Add("X-Frame-Options", "SAMEORIGIN")
 	c.Response.Out.Header().Add("X-XSS-Protection", "1; mode=block")
 	c.Response.Out.Header().Add("X-Content-Type-Options", "nosniff")
+	c.Response.Out.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Response.Out.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	fc[0](c, fc[1:]) // Execute the next filter stage.
 }
