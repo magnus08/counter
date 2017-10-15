150941a86118295964d324dd90cc5550dfd60514
Magnus Andersson <magnus.andersson@learnifier.com>
2017-10-15 19:22:32 +0200

    Cors.

--- app/init.go
+++ app/init.go
@@ -16,6 +16,8 @@ func init() {
 	// Filters is the default set of global filters.
 	revel.Filters = []revel.Filter{
 		revel.PanicFilter,             // Recover from panics and display an error page instead.
+		HeaderFilter,                  // Add some security based headers
+		// CORSFilter,                    // Cross Origin Resource Sharing         <-----
 		revel.RouterFilter,            // Use the routing table to select the right Action
 		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
 		revel.ParamsFilter,            // Parse parameters into Controller.Params.
@@ -23,7 +25,6 @@ func init() {
 		revel.FlashFilter,             // Restore and write the flash cookie.
 		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
 		revel.I18nFilter,              // Resolve the requested language
-		HeaderFilter,                  // Add some security based headers
 		revel.InterceptorFilter,       // Run interceptors around the action.
 		revel.CompressFilter,          // Compress the result.
 		revel.ActionInvoker,           // Invoke the action.
@@ -48,6 +49,13 @@ var HeaderFilter = func(c *revel.Controller, fc []revel.Filter) {
 	c.Response.Out.Header().Add("X-Content-Type-Options", "nosniff")
 	c.Response.Out.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Response.Out.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	c.Response.Out.Header().Set("Access-Control-Allow-Headers",
+					"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	// Stop here for a Preflighted OPTIONS request.
+	if c.Request.Method == "OPTIONS" {
+					return
+	}
 
 	fc[0](c, fc[1:]) // Execute the next filter stage.
 }
